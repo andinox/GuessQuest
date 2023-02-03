2e8644125f091784ce79a049d891702928f52a76
AnDiNoX <56691495+andinox@users.noreply.github.com>
2023-02-03 18:54:49 +0100

    0.1

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function sleep(ms) {
 document.getElementById("contenant_conn").addEventListener("click", async () => {
     document.getElementById("contenant_conn").classList.add("none");
     document.getElementById("form_conn").classList.remove("none");
+   // await sleep(200)
+    document.getElementById("pseudonim").classList.remove("invi");
     await sleep(400);
     document.getElementById("2ndpart").classList.add("test2");
+    await sleep(400);
+    document.getElementsByClassName("valide")[0].classList.remove("validew")
 })
