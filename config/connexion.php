68f21a0ba8018a8f1f0d65589bdb80a7b93bfa25
SADES <quentinamar95@gmail.com>
2023-03-08 18:43:23 +0100

    ajout page crea compte et changement de mdp

---
+++ config/connexion.php
@@ -0,0 +1,28 @@
+<?php
+class Connexion {
+  // les attributs static caractéristiques de la connexion
+  static private $hostname = "localhost";//"projets.iut-orsay.fr";
+  static private $database = "prj-s4-guestquest";
+  static private $login = "prj-s4-guestquest";//"sae-s3-lngbazo";
+  static private $password = "LFHVaNViNAOOwMaf";//"qnnlaa2308";
+
+  static private $tabUTF8 = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+
+  // l'attribut static qui matérialisera la connexion
+  static private $pdo;
+
+  // le getter public de cet attribut
+  static public function pdo() {return self::$pdo;}
+
+  // la fonction static de connexion qui initialise $pdo et lance la tentative de connexion
+  static public function connect()  {
+    $h = self::$hostname; $d = self::$database; $l = self::$login; $p = self::$password; $t = self::$tabUTF8;
+    try {
+    	self::$pdo = new PDO("mysql:host=$h;dbname=$d",$l,$p,$t);
+    	self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+    	echo "erreur de connexion : ".$e->getMessage()."<br>";
+    }
+  }
+}
+?>
