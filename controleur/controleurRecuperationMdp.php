10f9c044d50bbebf3f03f779ada0c9e728f97ca2
xWelloZ <alpha.sapin@gmail.com>
2023-04-17 22:06:45 +0200

    Ajout AdminListeReport Pas fini
    Ajout AdminListeReport Pas fini + contact dans footer

--- controleur/controleurRecuperationMdp.php
+++ controleur/controleurRecuperationMdp.php
@@ -9,4 +9,26 @@ class controleurRecuperationMdp {
         include("./vue/footer.html");
     }
 
+    public static function afficherQuestionMdp($pseudo){
+        $id_question = utilisateur::getId_QuestionRecupByPseudo($pseudo);
+        $question = utilisateur::getQuestionById_QuestionRecup($id_question);
+        // Afficher la question
+    }
+
+    public static function verifieReponseQuestionMdp($pseudo, $reponseDonnee){
+        $id_question = utilisateur::getId_QuestionRecupByPseudo($pseudo);
+        $reponse = utilisateur::getReponseById_QuestionRecup($id_question);
+
+        if($reponse == $reponseDonnee)
+            // Nouvelle page pour changer de MDP
+        else
+            // Afficher "Mauvaise réponse, si vous ne vous rappelez plus de votre réponse, contactez le support (lien vers la page de contact)"
+
+    }
+
+    public static function changerMdp($pseudo, $mdp){
+        $id_utilisateur = utilisateur::getId_UtilisateurByPseudo($pseudo);
+        utilisateur::updateMdp($id_utilisateur, $mdp);
+    }
+
 }
