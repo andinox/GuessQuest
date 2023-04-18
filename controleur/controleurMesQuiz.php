93c9f4294ca130e96d27f89cfc047f1c52cdf102
AnDiNoX <56691495+andinox@users.noreply.github.com>
2023-04-18 22:05:53 +0200

    4

--- controleur/controleurMesQuiz.php
+++ controleur/controleurMesQuiz.php
@@ -4,6 +4,7 @@ class controleurMesQuiz {
     public static function affiche() {
         $titre = "Mes Quiz";
         include("./vue/debut.php");
+        include "./vue/mesquiz/homequiz.html";
         include("./vue/footer.html");
     }
 }
